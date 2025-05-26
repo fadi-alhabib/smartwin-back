c43a4f995e72c283b44964910342da44c42e263f
jesus is king <fadi.b.alhabib@gmail.com>
2025-05-26 14:06:25 +0300

    final

--- app/Http/Controllers/Api/Gateway/MtnPaymentController.php
+++ app/Http/Controllers/Api/Gateway/MtnPaymentController.php
@@ -172,9 +172,9 @@ class MtnPaymentController extends Controller
         } elseif ($resBody["Errno"] == 404) {
             return response()->json(["message" => "ليس لديك حساب في mtn cash"], 400);
         } elseif ($resBody["Errno"] == 661) {
-            return response()->json(["message" => "لقد انتهى وقت كود التفعيل"]);
+            return response()->json(["message" => "لقد انتهى وقت كود التفعيل"], 400);
         } elseif ($resBody["Errno"] == 662) {
-            return response()->json(["message" => "كود التفعيل خاطئ يرجى التحقق منه و إعادة المحاولة"]);
+            return response()->json(["message" => "كود التفعيل خاطئ يرجى التحقق منه و إعادة المحاولة"], 400);
         } else {
             return response()->json(["message" => "لقد حدث خطأ يرجى إعادة المحاولة", $res->json()], 400);
         }
